53187cd73ffc68b1056085c7621918337f9e234a
tabetrahan <trahantet@gmail.com>
2021-02-04 17:00:32 -0500

    adding play function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function start() {
   const welcomeMessage = `The year is 2050.  Covid-19 has just been eradicated and you are now aloud to go out! \n You meet your friends at your favorite spot for ONE drink.  As you finish, your friends ask if the night should continue... \n\nDo you want to continue your night out?`;
   let answer = await ask(welcomeMessage);
 
-  if (answer === 'no'){
+  if (answer === 'no') {
     console.log('Enjoy your lame night in!');
     process.exit();
   } else {
@@ -27,52 +27,52 @@ async function start() {
 //--------------------------------- TABI + OLIVES CODE ------------------------------------------------
 
 //  start of player object... 
-let player {
-  name : [],
-  inventory : [],
-  currentRoom : [],
-  susPoints : []
+let player = {
+  name: [],
+  inventory: [],
+  currentRoom: [],
+  susPoints: []
 
   //moveRoom : function() {}
 }
 
 // room lookup table object
 const rooms = {
-'radio bean' : radioBean,
-'three needs' : threeNeeds,
-'pool room' : threeNeedsPoolRoom,
-'food cart' : foodCart,
-'finnegans' : finnegans,
-'deli' : deli126,
-'deli 126' : deli126,
-'red square' : redSquare,
-'kkd' : kkd,
-'kountry kart deli' : kkd,
-'jps' : jps,
+  'radio bean': radioBean,
+  'three needs': threeNeeds,
+  'pool room': threeNeedsPoolRoom,
+  'food cart': foodCart,
+  'finnegans': finnegans,
+  'deli': deli126,
+  'deli 126': deli126,
+  'red square': redSquare,
+  'kkd': kkd,
+  'kountry kart deli': kkd,
+  'jps': jps,
 }
 
 // Room class. including name, description inventory, directionals and will have actions available later
 class RoomClass {
-constructor(name, description, inventory, north, east, south, west) {
-this.name = name
-this.description = description 
-this.inventory = inventory
-this.north = north || null
-this.east = east || null
-this.south = south || null
-this.west = west || null
-}
-//will need "if" drop item - add to inventory
+  constructor(name, description, inventory, north, east, south, west) {
+    this.name = name
+    this.description = description
+    this.inventory = inventory
+    this.north = north || null
+    this.east = east || null
+    this.south = south || null
+    this.west = west || null
+  }
+  //will need "if" drop item - add to inventory
 }
 
 //  user actions look table object
 
 let userAction = {
-move: ['go', 'walk']
-use : ['use', 'play', 'get', 'drink', 'eat'] //tip?
-take : ['take', 'grab', 'order']
-open : ['open', 'unlock']
-drop : ['drop']
+  move: ['go', 'walk'],
+  use: ['use', 'play', 'get', 'drink', 'eat'], //tip?
+  take: ['take', 'grab', 'order'],
+  open: ['open', 'unlock'],
+  drop: ['drop'],
 }
 
 // inventory look up table
@@ -86,28 +86,39 @@ let invObjects = {
 }
 
 //  inventory class
-class InvObjectsClass{
-  constructor(name, description, startingRoom, currentRoom , action) {
+class InvObjectsClass {
+  constructor(name, description, startingRoom, currentRoom, action) {
     this.name = name
     this.description = description
     this.startingRoom = startingRoom
     this.currentRoom = currentRoom || startingRoom // may need an inventory room for objects to be in... 
     this.action = action || [] // action array
   }
-  
+
 }
 
-const map = new InvObjectsClass(map, "this is a map of Burlington's downtown area", threeNeedsPoolRoom, threeNeedsPoolRoom, [use, take, drop])
+const map = new InvObjectsClass(map, "this is a map of Burlington's downtown area", threeNeedsPoolRoom, undefined, [use, take, drop])
 
 
 //  need another async (?) play function... that 
 //  1)takes a user input 
 //  2) sanitizes it into multiple parts.. 
-  // one part will need action = userInputAction
-  // second part will need to be direction/object = userInputObject
+// one part will need action = userInputAction
+// second part will need to be direction/object = userInputObject
 //  3) jump into conditional ifs based on userInput.. where we do what the user said or... console.log(i dont know what your talking about)
-//  4) at end want to jump to very top of play function...
+//  4) at end want to jump to very top of play function...  will take you to top of function.. return function(); recursive loop 
 
+
+async function play (){
+  //  sanitize
+  let input = await ask(`Current Room: ${player.currentRoom} \nWhat would you like to do....\n\n>_`);
+  let inputSanitize = input.trim().toLowerCase(); 
+  let inputArray = inputSanitize.split(' ');
+  let inputAction = inputArray[0];
+  let inputObject = inputArray[1]; // Can also be direction if action is go , walk, etc.
+
+
+}
 
 
 
