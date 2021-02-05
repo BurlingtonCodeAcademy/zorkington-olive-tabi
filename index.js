bd2390322deb2e810fd9f32ad7f993de13964f2b
tabetrahan <trahantet@gmail.com>
2021-02-05 11:35:35 -0500

    got things moving

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -13,124 +16,187 @@ async function start() {
   const welcomeMessage = `The year is 2050.  Covid-19 has just been eradicated and you are now aloud to go out! \n You meet your friends at your favorite spot for ONE drink.  As you finish, your friends ask if the night should continue... \n\nDo you want to continue your night out?`;
   let answer = await ask(welcomeMessage);
 
-  if (answer === 'no') {
-    console.log('Enjoy your lame night in!');
+  if (answer === "no") {
+    console.log("Enjoy your lame night in!");
     process.exit();
   } else {
-    console.log('You exit the Radio Bean and find yourself at the intersection of Pearl and N.Winooski.  To the east is the hill to UVM and to the north a familiar sight.....')
-    let firstDirection = await ask("Where would you like to go?")
+    console.log(
+      "You exit the Radio Bean and find yourself at the intersection of Pearl and N.Winooski.  To the east is the hill to UVM and to the north a familiar sight....."
+    );
+    let firstDirection = await ask("Where would you like to go?");
     console.log(firstDirection);
-    process.exit();
+    player.currentRoom = "Radio Bean";
+    return play();
   }
 }
 
 //--------------------------------- TABI + OLIVES CODE ------------------------------------------------
 
-//  start of player object... 
+//  start of player object...
 let player = {
   name: [],
   inventory: [],
   currentRoom: [],
-  susPoints: []
+  susPoints: [],
 
   //moveRoom : function() {}
-}
-
-// room lookup table object
-const rooms = {
-  'radio bean': radioBean,
-  'three needs': threeNeeds,
-  'pool room': threeNeedsPoolRoom,
-  'food cart': foodCart,
-  'finnegans': finnegans,
-  'deli': deli126,
-  'deli 126': deli126,
-  'red square': redSquare,
-  'kkd': kkd,
-  'kountry kart deli': kkd,
-  'jps': jps,
-}
+};
 
 // Room class. including name, description inventory, directionals and will have actions available later
 class RoomClass {
   constructor(name, description, inventory, north, east, south, west) {
-    this.name = name
-    this.description = description
-    this.inventory = inventory
-    this.north = north || null
-    this.east = east || null
-    this.south = south || null
-    this.west = west || null
+    this.name = name;
+    this.description = description;
+    this.inventory = inventory;
+    this.north = north || null;
+    this.east = east || null;
+    this.south = south || null;
+    this.west = west || null;
   }
   //will need "if" drop item - add to inventory
-}
-const room = new InvObjectsClass(threeNeeds, "The three needs blah blah blah", [water, shot], threeNeedsPoolRoom, null, null, west)
-
-//  user actions look table object
-
-let userAction = {
-  move: ['go', 'walk'],
-  use: ['use', 'play', 'get', 'drink', 'eat'], //tip?
-  take: ['take', 'grab', 'order'],
-  open: ['open', 'unlock'],
-  drop: ['drop'],
-}
-
-// inventory look up table
-let invObjects = {
-  'map': map,
-  'water': water,
-  'sparkling water': sparkWater,
-  'shot': alcohol,
-  'food': food,
-  'sandwich': prize
 }
 
 //  inventory class
 class InvObjectsClass {
   constructor(name, description, startingRoom, currentRoom, action) {
-    this.name = name
-    this.description = description
-    this.startingRoom = startingRoom
-    this.currentRoom = currentRoom || startingRoom // may need an inventory room for objects to be in... 
-    this.action = action || [] // action array
+    this.name = name;
+    this.description = description;
+    this.startingRoom = startingRoom;
+    this.currentRoom = currentRoom || startingRoom; // may need an inventory room for objects to be in...
+    this.action = action || []; // action array
   }
-
 }
 
-const mapObj = new InvObjectsClass(map, "this is a map of Burlington's downtown area", threeNeedsPoolRoom, undefined, [use, take, drop])
+
+//  user actions look table object
+
+let userAction = {
+  move: ["go", "walk"],
+  use: ["use", "play", "get", "drink", "eat"], //tip?
+  take: ["take", "grab", "order"],
+  open: ["open", "unlock"],
+  drop: ["drop"],
+};
 
 
-//  need another async (?) play function... that 
-//  1)takes a user input 
-//  2) sanitizes it into multiple parts.. 
+
+//  objects... maybe cant include locations because of variable shadowing
+//  need to define things in objects AFTER we define them... maybe dont need these pieces.. will need to decide
+const water = new InvObjectsClass(
+    "water",
+    "water description...",
+    undefined,
+    undefined,
+    []
+  );
+  const shot = new InvObjectsClass(
+    "shot",
+    "shot description...",
+    undefined,
+    undefined,
+    []
+  );
+
+// inventory look up table
+let invObjects = {
+    //   'map': map,
+    water: water,
+    //   'sparkling water': sparkWater,
+    shot: shot,
+    //   'food': food,
+    //   'sandwich': prize
+  };
+
+
+const threeNeedsPoolRoom = new RoomClass(
+  "threeNeeds pool room",
+  "The three needs pool room blah blah blah",
+  [],
+  null,
+  null,
+  null,
+  null
+);
+const threeNeeds = new RoomClass(
+  "threeNeeds",
+  "The three needs blah blah blah",
+  [],
+  threeNeedsPoolRoom,
+  null,
+  null,
+  null
+);
+const radioBean = new RoomClass(
+  "the radio bean",
+  "The radio bean",
+  [invObjects.water, invObjects.shot],
+  threeNeeds,
+  null,
+  null,
+  null
+);
+
+
+
+// room lookup table object
+const rooms = {
+  "radio bean": radioBean,
+  "three needs": threeNeeds,
+  "pool room": threeNeedsPoolRoom,
+  //   'food cart': foodCart,
+  //   'finnegans': finnegans,
+  //   'deli': deli126,
+  //   'deli 126': deli126,
+  //   'red square': redSquare,
+  //   'kkd': kkd,
+  //   'kountry kart deli': kkd,
+  //   'jps': jps
+};
+
+
+
+// ------------------------------------- PLAN ------------------------------------------------------
+//  need another async (?) play function... that
+//  1)takes a user input
+//  2) sanitizes it into multiple parts..
 // one part will need action = userInputAction
 // second part will need to be direction/object = userInputObject
 //  3) jump into conditional ifs based on userInput.. where we do what the user said or... console.log(i dont know what your talking about)
-//  4) at end want to jump to very top of play function...  will take you to top of function.. return function(); recursive loop 
+//  4) at end want to jump to very top of play function...  will take you to top of function.. return function(); recursive loop
 
-
-async function play (){
+async function play() {
   //  sanitize
-  let input = await ask(`Current Room: ${player.currentRoom} \nWhat would you like to do....\n\n>_`);
-  let inputSanitize = input.trim().toLowerCase(); 
-  let inputArray = inputSanitize.split(' ');
+  let input = await ask(
+    `Current Room: ${player.currentRoom} \nWhat would you like to do....\n\n>_`
+  );
+  let inputSanitize = input.trim().toLowerCase();
+  let inputArray = inputSanitize.split(" ");
   let inputAction = inputArray[0];
   let inputObject = inputArray[1]; // Can also be direction if action is go , walk, etc.
 
+  //  if user types end --> end
+  if (inputSanitize === "end") {
+    process.exit();
 
+    // if user types inventory show inventory
+
+    // if user wants to move lets move
+  } else if (userAction.move.includes(inputAction)) {
+    if (inputObject.toLowerCase() === "north") {
+      if (rooms[player.currentRoom.toLowerCase()].north) {
+        // this is to find rooms northern room connection
+        roomToNorth = rooms[player.currentRoom.toLowerCase()].north;
+        player.currentRoom = roomToNorth.name;
+        console.log(roomToNorth.description);
+        console.log(`changing currentRoom to ${roomToNorth.name}`);
+      } else  {console.log("you can't go that way");};
+    } else if (inputObject.toLowerCase() === "east") {
+    } else if (inputObject.toLowerCase() === "south") {
+    } else {
+    }
+  } else {console.log("you can't do that");}  
+  return play();
 }
-
-}
-
-
-
-
-
-
-
-
-
 /*
 
 //  structure hypothetical
@@ -253,3 +319,5 @@ if (userInput === 'i'){
         - park shortcut 
         - cocktail at deli 126 too expensive?
         - redsquare die case: pick wrong song, attacked by college mob.. or bro pick up.. or spill drink
+
+        */
